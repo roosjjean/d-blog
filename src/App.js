c4d4809879168b681c848ba0ce4793bb5d75bc4a
jeanroosjehu-jrjc <jeanroosjehu@gmail.com>
2023-09-17 16:40:11 -0400

    404 Page setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Create from "./Create";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import NotFound from "./NotFound";
 
 const App = () => {
   return (
@@ -19,6 +20,9 @@ const App = () => {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>That Page Cannot Be Found</p>
+            <br />
+            <Link to="/">Back To Home Page</Link>
+        </div>
+    );
+}
+ 
+export default NotFound;
